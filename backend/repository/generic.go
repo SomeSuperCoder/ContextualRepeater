fe865f8c116bc01d43edda1612dc50fb86322db7
SomeSuperCoder <ithelper9@gmail.com>
2025-11-02 14:36:41 +0300

    Removed extra method-function wrappers

--- backend/repository/generic.go
+++ backend/repository/generic.go
@@ -21,35 +21,73 @@ func NewGenericRepo[T any, U any](database *mongo.Database, collectionName strin
 }
 
 func (r *GenericRepo[T, U]) FindPaged(ctx context.Context, page, limit int64) ([]T, int64, error) {
-	return FindPaged[T](ctx, r.Collection, page, limit)
+	var values = []T{}
+
+	// Set pagination options
+	skip := (page - 1) * limit
+	opts := options.Find()
+	opts.SetLimit(limit)
+	opts.SetSkip(skip)
+	opts.SetSort(bson.M{"created_at": -1})
+
+	// Init a cursor
+	cursor, err := r.Collection.Find(ctx, bson.M{}, opts)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer cursor.Close(ctx)
+
+	// Extract records
+	err = cursor.All(ctx, &values)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// Get total count
+	count, err := r.Collection.CountDocuments(ctx, bson.M{})
+
+	return values, count, err
 }
 
 func (r *GenericRepo[T, U]) Find(ctx context.Context) ([]T, error) {
-	return Find[T](ctx, r.Collection)
+	return FindWithFilter[T](ctx, r.Collection, struct{}{})
 }
 
 func (r *GenericRepo[T, U]) Create(ctx context.Context, value *T) (bson.ObjectID, error) {
-	return Create(ctx, r.Collection, value)
+	res, err := r.Collection.InsertOne(ctx, value)
+	objID, _ := res.InsertedID.(bson.ObjectID)
+	return objID, err
 }
 
 func (r *GenericRepo[T, U]) GetByID(ctx context.Context, id bson.ObjectID) (*T, error) {
-	return GetByID[T](ctx, r.Collection, id)
+	return GetBy[T](ctx, r.Collection, "_id", id)
+
 }
 
 func (r *GenericRepo[T, U]) Update(ctx context.Context, id bson.ObjectID, update *U) error {
-	return Update(ctx, r.Collection, id, update)
+	res := r.Collection.FindOneAndUpdate(ctx, bson.M{
+		"_id": id,
+	}, bson.M{
+		"$set": update,
+	})
+
+	return res.Err()
 }
 
 func (r *GenericRepo[T, U]) Delete(ctx context.Context, id bson.ObjectID) error {
-	return Delete(ctx, r.Collection, id)
+	_, err := r.Collection.DeleteOne(ctx, bson.M{
+		"_id": id,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // =============================
-
-func Find[T any](ctx context.Context, c *mongo.Collection) ([]T, error) {
-	return FindWithFilter[T](ctx, c, struct{}{})
-}
-
+// Helper funcs
+// =============================
 func FindWithFilter[T any](ctx context.Context, c *mongo.Collection, filter any) ([]T, error) {
 	var values = []T{}
 
@@ -69,41 +107,6 @@ func FindWithFilter[T any](ctx context.Context, c *mongo.Collection, filter any)
 	return values, err
 }
 
-func FindPaged[T any](ctx context.Context, c *mongo.Collection, page, limit int64) ([]T, int64, error) {
-	var values = []T{}
-
-	// Set pagination options
-	skip := (page - 1) * limit
-	opts := options.Find()
-	opts.SetLimit(limit)
-	opts.SetSkip(skip)
-	opts.SetSort(bson.M{"created_at": -1})
-
-	// Init a cursor
-	cursor, err := c.Find(ctx, bson.M{}, opts)
-	if err != nil {
-		return nil, 0, err
-	}
-	defer cursor.Close(ctx)
-
-	// Extract records
-	err = cursor.All(ctx, &values)
-	if err != nil {
-		return nil, 0, err
-	}
-
-	// Get total count
-	count, err := c.CountDocuments(ctx, bson.M{})
-
-	return values, count, err
-}
-
-func Create(ctx context.Context, c *mongo.Collection, value any) (bson.ObjectID, error) {
-	res, err := c.InsertOne(ctx, value)
-	objID, _ := res.InsertedID.(bson.ObjectID)
-	return objID, err
-}
-
 func GetBy[T any](ctx context.Context, c *mongo.Collection, key string, value any) (*T, error) {
 	var got T
 
@@ -115,29 +118,4 @@ func GetBy[T any](ctx context.Context, c *mongo.Collection, key string, value an
 	}
 
 	return &got, err
-}
-
-func GetByID[T any](ctx context.Context, c *mongo.Collection, id bson.ObjectID) (*T, error) {
-	return GetBy[T](ctx, c, "_id", id)
-}
-
-func Update(ctx context.Context, c *mongo.Collection, id bson.ObjectID, update any) error {
-	res := c.FindOneAndUpdate(ctx, bson.M{
-		"_id": id,
-	}, bson.M{
-		"$set": update,
-	})
-
-	return res.Err()
-}
-
-func Delete(ctx context.Context, c *mongo.Collection, id bson.ObjectID) error {
-	_, err := c.DeleteOne(ctx, bson.M{
-		"_id": id,
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
